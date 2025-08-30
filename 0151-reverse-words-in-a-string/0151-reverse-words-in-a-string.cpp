0d82ada9ed05de8ab39483f596aeb051129065c6
Ayush  Bhatt <111ayushh@gmail.com>
2025-08-30 18:14:25 +0530

    Time: 0 ms (100%), Space: 9.9 MB (90.48%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,7 +3,7 @@ public:
     string reverseWords(string s) {
 
         //remove leading spaces
-        
+
         // int i = 0;
         // while(s[i] == ' '){
         //     s.erase(0,i);
@@ -21,9 +21,23 @@ public:
         //     s.erase(j-i+1);
         //     j--;
         // }
-        while(!s.empty() && s.back() == ' ') {
-            s.pop_back();
+        // while(!s.empty() && s.back() == ' ') {
+        //     s.pop_back();
+        // }
+
+        // remove leading spaces
+        // int i = 0;
+        // while(i < s.size() && s[i] == ' ') {
+        //     s.erase(0, 1);
+        // }
+
+        // remove trailing spaces
+        int j = s.length() - 1;
+        while(j >= 0 && s[j] == ' ') {
+            s.erase(j, 1);
+            j = s.length() - 1;
         }
+
 
 
 
