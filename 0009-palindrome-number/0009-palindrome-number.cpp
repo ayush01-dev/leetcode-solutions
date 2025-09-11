888e7625c377f2091737b71d15fc72aca4a1b1fb
Ayush  Bhatt <111ayushh@gmail.com>
2025-09-11 05:54:10 +0530

    Time: 4 ms (32.3%), Space: 8.6 MB (40.44%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long rev = 0;
+        int temp = x;
+        while(x != 0){
+            int reminder = x % 10;
+            rev = rev*10 + reminder;
+            x = x/10;
+
+        } 
+
+        if(rev == temp){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
