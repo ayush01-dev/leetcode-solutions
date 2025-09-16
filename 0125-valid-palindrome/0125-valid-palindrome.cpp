2b022146152324fe5c7c1e690a0206872bd93934
Ayush  Bhatt <111ayushh@gmail.com>
2025-09-17 05:21:16 +0530

    Time: 0 ms (100%), Space: 9.9 MB (79.11%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            // Move left pointer until we find an alphanumeric character
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            // Move right pointer until we find an alphanumeric character
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            // Compare lowercase versions
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
