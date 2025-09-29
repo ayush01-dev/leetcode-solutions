a6e70248206e306ddc34d087c8ed14d17056755e
Ayush  Bhatt <111ayushh@gmail.com>
2025-09-29 06:05:39 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.86%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        
+
+        //step 1 find longest prifix match
+       int index = -1;
+       int n = nums.size();
+       for(int i = n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+       }
+
+       if(index == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+       }
+
+
+        //step 2 find x (but the smallest one so that you stay close)
+        for(int i = n-1; i>index; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+
+        //step 3
+        reverse(nums.begin() + index + 1, nums.end());
+
+    return;  
     }
 };
