f0251fd77a80ed96b55127ff900e87ede1bcd14c
Ayush  Bhatt <111ayushh@gmail.com>
2025-10-02 05:07:29 +0530

    Time: 3 ms (35.26%), Space: 63 MB (48.2%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        //BRUTE force got TLE
+        //int maxAr = INT_MIN;
+        //int area,breadth,length;
+        //     for(int i = 0; i < height.size(); i++){
+        //         for(int j = i+1; j < height.size(); j++){
+        //             breadth = min(height[i],height[j]);
+        //             length = j - i;  // instead of length = j;
+        //             area = length * breadth;
+        //             maxAr = max(maxAr,area);
+
+        //         }
+        //     }
+        // return maxAr;
+
+    //using two pointers approach
+        int maxAr = INT_MIN;
+        int area,breadth,length;
+        int start = 0;
+        int end = height.size()-1;
+        while(start <= end){
+            breadth = min(height[start],height[end]);
+            length = end - start;
+            area = length * breadth;
+            maxAr = max(maxAr,area);
+            if(height[start] < height[end]){
+                start++;
+            }
+            else{
+                end--;
+            }
+            
+        }
+    return maxAr;
+    }
+};
